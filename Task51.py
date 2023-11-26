82d77e80ae2951c9547f5f2e132e34a2177b51e4
Efim Kubishkin <efimkub@mail.ru>
2023-11-26 22:10:00 +0300

    Add second part of Task 5.1: formula max algebraic power accuracy

--- Task51.py
+++ Task51.py
@@ -2,7 +2,10 @@ from math import cos, sin
 from scipy import integrate
 from prettytable import PrettyTable
 
-from Integration import create_interpolating_quadrature
+from Integration import (
+    create_interpolating_quadrature,
+    create_quadrature_max_algebraic_accuracy,
+)
 
 
 def rho(x: float) -> float:
@@ -53,7 +56,7 @@ def input_int(msg: str) -> int:
             print("Введено не целое число")
 
 
-if __name__ == "__main__":
+def print_results_int_quad() -> None:
     print(
         "Тема: Приближённое вычисление интегралов при помощи квадратурных "
         "формул Наивысшей Алгебраической Степени "
@@ -62,29 +65,50 @@ if __name__ == "__main__":
     left_bound = input_float("Введите левую границу интегрирования: ")
     right_bound = input_float("Введите левую границу интегрирования: ")
     n = input_int("Введите количество точек для построения КФ: ")
+
+    if n < 2:
+        print("Количество узлов обязано быть больше 2")
+        print_results_int_quad()
+
     print("ρ = cos(x)")
     print("f(x) = sin(x)")
 
-    preciseF = integrate.quad(lambda x: rho(x) * f(x), left_bound, right_bound)
-    i_quadF = create_interpolating_quadrature(left_bound, right_bound, n, f, rho)
+    precise_f = integrate.quad(lambda x: rho(x) * f(x), left_bound, right_bound)
+    i_quad_f = create_interpolating_quadrature(left_bound, right_bound, n, f, rho)
+    quad_max_alg_acc = create_quadrature_max_algebraic_accuracy(
+        left_bound, right_bound, n, f, rho
+    )
 
-    tableF = PrettyTable()
-    tableF.field_names = ["Метод", "Значение", "Погрешность"]
-    tableF.add_row(["Точное значение (SciPy)", preciseF[0], preciseF[1]])
-    tableF.add_row(["ИКФ", i_quadF, abs(i_quadF - preciseF[0])])
-    print(tableF)
+    table_f = PrettyTable()
+    table_f.field_names = ["Метод", "Значение", "Погрешность"]
+    table_f.add_row(["Точное значение (SciPy)", precise_f[0], precise_f[1]])
+    table_f.add_row(["ИКФ", i_quad_f, abs(i_quad_f - precise_f[0])])
+    table_f.add_row(["КФНАСТ", quad_max_alg_acc, abs(quad_max_alg_acc - precise_f[0])])
+    print(table_f)
 
     print(f"{80*'='}")
     print(f"f(x) = x^{n - 1}")
-    preciseN = integrate.quad(
+    precise_n = integrate.quad(
         lambda x: rho(x) * (x ** (n - 1)), left_bound, right_bound
     )
-    i_quadN = create_interpolating_quadrature(
+    i_quad_n = create_interpolating_quadrature(
+        left_bound, right_bound, n, lambda x: x ** (n - 1), rho
+    )
+    quad_max_alg_acc_n = create_quadrature_max_algebraic_accuracy(
         left_bound, right_bound, n, lambda x: x ** (n - 1), rho
     )
 
-    tableN = PrettyTable()
-    tableN.field_names = ["Метод", "Значение", "Погрешность"]
-    tableN.add_row(["Точное значение (SciPy)", preciseN[0], preciseN[1]])
-    tableN.add_row(["ИКФ", i_quadN, abs(i_quadN - preciseN[0])])
-    print(tableN)
+    table_n = PrettyTable()
+    table_n.field_names = ["Метод", "Значение", "Погрешность"]
+    table_n.add_row(["Точное значение (SciPy)", precise_n[0], precise_n[1]])
+    table_n.add_row(["ИКФ", i_quad_n, abs(i_quad_n - precise_n[0])])
+    table_n.add_row(["КФНАСТ", quad_max_alg_acc_n, abs(quad_max_alg_acc_n - precise_n[0])])
+    print(table_n)
+    print("Хотите повторить с другими данными?")
+    again = input("[Да, да, д, y, yes], чтобы продолжить: ")
+    if again in ["Да", "да", "д", "y", "yes"]:
+        print_results_int_quad()
+
+
+if __name__ == "__main__":
+    print_results_int_quad()
