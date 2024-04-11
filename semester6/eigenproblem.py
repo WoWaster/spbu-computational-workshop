3bcbc39cbb88f90306b49b29c69a9a389deb099f
Nikolai Ponomarev <wowasterdev@gmail.com>
2024-04-11 21:55:02 +0300

    Add solution to Task 5

--- semester6/eigenproblem.py
+++ semester6/eigenproblem.py
@@ -1,12 +1,8 @@
 from enum import Enum, auto
 from math import sqrt
-from typing import Union
 
 import numpy as np
-from numpy.linalg import LinAlgError
 from scipy import linalg
-
-from semester6 import utils
 
 
 class PartialEigenproblemMethod(Enum):
@@ -14,4 +10,68 @@ class PartialEigenproblemMethod(Enum):
     DotProductMethod = auto()
 
 
-def solve(a: np.nd)
+def _solve_power_method(a: np.ndarray, eps: float) -> (float, np.ndarray, int):
+    eigenvector = np.random.rand(a.shape[1])
+    prev_eigenvector = None
+    eigenvalue = None
+    prev_eigenvalue = None
+    counter = 0
+
+    while prev_eigenvalue is None or abs(eigenvalue - prev_eigenvalue) >= eps:
+        if eigenvalue is not None:
+            prev_eigenvalue = eigenvalue
+
+        prev_eigenvector = eigenvector
+        eigenvector = a @ eigenvector
+
+        eigenvalue = sqrt(
+            np.dot(eigenvector, eigenvector)
+            / np.dot(prev_eigenvector, prev_eigenvector)
+        )
+
+        eigenvector = eigenvector / linalg.norm(eigenvector, ord=2)
+
+        counter += 1
+
+    return eigenvalue, eigenvector, counter
+
+
+def _solve_dot_product_method(a: np.ndarray, eps: float) -> (float, np.ndarray, int):
+    eigenvector = np.random.rand(a.shape[1])
+    helper_eigenvector = np.random.rand(a.shape[1])
+    prev_eigenvector = None
+    eigenvalue = None
+    prev_eigenvalue = None
+    counter = 0
+
+    while prev_eigenvalue is None or abs(eigenvalue - prev_eigenvalue) >= eps:
+        if eigenvalue is not None:
+            prev_eigenvalue = eigenvalue
+
+        prev_eigenvector = eigenvector
+        eigenvector = a @ eigenvector
+
+        eigenvalue = np.dot(eigenvector, helper_eigenvector) / np.dot(
+            prev_eigenvector, helper_eigenvector
+        )
+
+        eigenvector = eigenvector / linalg.norm(eigenvector, ord=2)
+
+        helper_eigenvector = a.T @ helper_eigenvector
+        helper_eigenvector = helper_eigenvector / linalg.norm(helper_eigenvector, ord=2)
+
+        counter += 1
+
+    return eigenvalue, eigenvector, counter
+
+
+def solve_partial(
+    a: np.ndarray,
+    method: PartialEigenproblemMethod = PartialEigenproblemMethod.DotProductMethod,
+    eps: float = 10e-6,
+) -> (float, np.ndarray, int):
+    match method:
+        case PartialEigenproblemMethod.PowerMethod:
+            return _solve_power_method(a, eps)
+        case PartialEigenproblemMethod.DotProductMethod:
+            return  _solve_dot_product_method(a, eps)
